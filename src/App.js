260766e80f8a8f7b18679ab85f60f4dd0c555f26
phil-sorenson <phillip.sorenson31@gmail.com>
2022-12-23 02:48:44 -0600

    Deleted unnecessary original react files + Installed Axios + Created an async GET method function in our App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
 
 function App() {
+  
+  const [songs, setSongs] = useState([])
+
+  useEffect( ()=>{
+    getAllSongs()
+  },[]);
+  // Because the [] above is empty, the useEffect will only run one time
+    
+  async function getAllSongs() {
+      // Whenever we are making request with Axios, we want to create a variable to HOLD THE EVENTUAL RESPONSE/RESOLUTION of the promise 
+    const response = await axios.get('http://127.0.0.1:8000/music/');
+      // The api get request 'response' will be held in the response variable above
+    console.log(response.data)
+    setSongs(response.data)
+
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={()=> getAllSongs()}>Get All Songs</button>
     </div>
   );
 }
 
 export default App;
+
+//? Is there a keyboard shortcut to get cursor from the middle of tag(s) to the end of that line in order to just click 'enter' to get to next line 🤔
+//? How can I get my terminal to show me the speed at which my commands are loading
